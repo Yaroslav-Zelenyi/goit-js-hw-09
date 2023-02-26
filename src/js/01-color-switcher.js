7f8ac609eb4e980b054a180f012a9e448b071f29
Yaroslav-Zelenyi <zelenyiyaroslav@gmail.com>
2023-02-26 16:16:29 +0200

    Create js-09/01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intervalId = null;
 
+function randomColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function handleClickStart() {
+  intervalId = setInterval(randomColor, 1000);
+  startButton.disabled = true;
+}
+function handleClickStop() {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', handleClickStart);
+stopButton.addEventListener('click', handleClickStop);
